f4269792d33717b74e6c944fb44d805701229648
Jeremy Quinlan <jpeg747@gmail.com>
2013-03-07 19:13:35 -0500

    made recieved byte size bigger, display more info from server

--- TCPClient/Program.cs
+++ TCPClient/Program.cs
@@ -27,11 +27,11 @@ namespace TCPClient
                 strm.Write(data, 0, data.Length);
                 Console.WriteLine("Sent: {0}", msg);
 
-                data = new Byte[256];
+                data = new Byte[2048];
                 String rspData = String.Empty;
                 Int32 bytes = strm.Read(data, 0, data.Length);
                 rspData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-                rspIP = rspData.Substring(rspData.IndexOf("\r") - 9, 9);
+                rspIP = rspData.Substring(rspData.IndexOf("\r\n") - 9, 9);
                 Console.WriteLine("Received: {0}", rspData);
                 strm.Close();
                 myClient.Close();
@@ -41,11 +41,9 @@ namespace TCPClient
                 Console.WriteLine("SocketExecption: {0}", e);
             }
 
-            for (int i = 0; i < 3; i++)
+            for (int i = 1; i <= 3; i++)
             {
-                Random rand = new Random();
-                msg = "GET " + rand.Next(1,20) + "\r\n";
-
+                msg = "GET " + (3*i) + "\r\n";
 
                 try
                 {
@@ -55,11 +53,11 @@ namespace TCPClient
                     strm.Write(data, 0, data.Length);
                     Console.WriteLine("Sent: {0}", msg);
 
-                    data = new Byte[256];
+                    data = new Byte[2048];
                     String rspData = String.Empty;
                     Int32 bytes = strm.Read(data, 0, data.Length);
                     rspData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
-                    Console.WriteLine("Received: {0}", rspData);
+                    Console.WriteLine("Received: {0} {1}", "(From: "+rspIP+", Port: "+QuotePort+")", rspData);
                     strm.Close();
                     myClient.Close();
                 }
